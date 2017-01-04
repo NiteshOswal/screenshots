71759efc8544d8276007165ca22ba992f4a61376
Nitesh Oswal <nit.oswal@gmail.com>
2017-01-05 01:06:27 +0530

    Initial commit, basic connect architecture

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const connect = require('connect');
-const qs 
-const path = require('path');
-const http = require('http');
-const serveStatic = require('serve-static');
-const webshot = require('webshot');
-const isUrl = require('is-url');
-const md5 = require('md5');
+const url = require('url');
+const qs = require('qs');
 
 const app = connect();
+
+app.use(function query(req, res, next) {
+    console.log(url.parse(req.url));
+    next();
+});
+
+app.use('/', function(req, res) {
+    res.end(JSON.stringify(req.query));
+});
 
 http.createServer(app).listen(4000);
